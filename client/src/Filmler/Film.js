d17849b2e86d81c150486fac20b3c9357b2294b1
KrieqqeR <burakcevizli@gmail.com>
2023-08-07 22:47:27 +0300

    Güncel hali

--- client/src/Filmler/Film.js
+++ client/src/Filmler/Film.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom/';
 
 export default function Film(props) {
   const [movie, setMovie] = useState();
-
-  let id = props.id;
+  const params = useParams()
+  let id = params.id;
   // URL'den alınan :id parametresini bu değişkene aktarın
 
   useEffect(() => {
